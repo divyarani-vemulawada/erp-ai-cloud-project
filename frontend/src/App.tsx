6afc0bb27d845ff453c0c723a3387648679f80d5
Sai Tharun Kishore <simplesaitharun@gmail.com>
2026-06-16 21:08:46 +0530

    Implemented HR employee list module with mock data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Employees from './pages/HR/Employees'
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/employees" replace />} />
       <Route path="/employees" element={<Employees />} />
     </Routes>
   )
